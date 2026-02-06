4fb2100d91188090670c6ab4879195355a014d63
kurichan12 <io.sono.un.bugiardo@gmail.com>
2026-02-06 16:44:46 +0900

    スマホ対応

---
+++ app/components/AdBanner.tsx
@@ -0,0 +1,8 @@
+export default function AdBanner() {
+  return (
+    <div className="w-full h-[100px] bg-gray-200 border-2 border-dashed border-gray-400 flex items-center justify-center text-gray-500 my-4">
+      {/* ここにGoogle AdSenseなどのコードが入ります */}
+      <p>広告スペース（課金で非表示）</p>
+    </div>
+  );
+}
